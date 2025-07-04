8f8b68dc67e5a0b72b2bddbe345cfc94293a7c9c
Arne Goeteyn <arne@datavillage.me>
2025-07-04 09:32:58 +0200

    add pre wrap component to route page

--- src/components/routes/route-information.tsx
+++ src/components/routes/route-information.tsx
@@ -1,5 +1,5 @@
 import { Route } from "@/lib/routes/types";
-import { Text } from "@mantine/core";
+import { Box, Text, Textarea } from "@mantine/core";
 
 type Props = {
   route: Route;
@@ -10,8 +10,12 @@ const noInformationText = "No information provided about this route.";
 export default function RouteInformation(props: Props) {
   return (
     <div>
-      <Text>{props.route.comment ?? noInformationText}</Text>
-      <Text fs={"italic"}>{props.route.beta}</Text>
+      <Text pb={4} style={{ whiteSpace: "pre-wrap" }}>
+        {props.route.comment ?? noInformationText}
+      </Text>
+      <Text style={{ whiteSpace: "pre-wrap" }} fs={"italic"}>
+        {props.route.beta}
+      </Text>
     </div>
   );
 }
