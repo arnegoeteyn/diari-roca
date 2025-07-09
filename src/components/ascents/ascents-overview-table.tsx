81cccc9a5f13d9ac6c125b7817f38e336a669fb2
Arne Goeteyn <arne@datavillage.me>
2025-07-09 15:12:13 +0200

    simple ascents table

---
+++ src/components/ascents/ascents-overview-table.tsx
@@ -0,0 +1,65 @@
+import { AscentOverview } from "@/lib/routes/types";
+import { Pagination, Stack, Table, Text } from "@mantine/core";
+import { useState } from "react";
+import AscentBadge from "../ascents/ascent-badge";
+import RouteBreadcrumbs from "../routes/route-breadcrumbs";
+import { RouteContextProvider } from "@/contexts/route-context";
+
+type Props = {
+  ascents: AscentOverview[];
+};
+
+const ROUTES_PER_PAGE = 25;
+
+export default function AscentsOverviewTable(props: Props) {
+  const [page, setPage] = useState<number>(0);
+
+  const shownAscents = () => {
+    return props.ascents.slice(
+      page * ROUTES_PER_PAGE,
+      (page + 1) * ROUTES_PER_PAGE
+    );
+  };
+  console.log(props.ascents);
+  console.log(shownAscents());
+
+  return (
+    <Stack>
+      <Table stickyHeader stickyHeaderOffset={60} striped>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Date</Table.Th>
+            <Table.Th>Route</Table.Th>
+            <Table.Th>Area</Table.Th>
+            <Table.Th>Kind</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {shownAscents().map((ascent) => (
+            <Table.Tr key={ascent.ascent.id}>
+              <Table.Td align="left" className="font-medium">
+                <Text>{ascent.ascent.date}</Text>
+              </Table.Td>
+              <Table.Td>
+                <Text>{`${ascent.route.name} (${ascent.route.grade})`}</Text>
+              </Table.Td>
+              <Table.Td>
+                <RouteContextProvider routeId={ascent.route.id.toString()}>
+                  <RouteBreadcrumbs />
+                </RouteContextProvider>
+              </Table.Td>
+              <Table.Td>
+                <AscentBadge ascent={ascent.ascent} />
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+      <Pagination
+        value={page + 1}
+        onChange={(p) => setPage(p - 1)}
+        total={Math.ceil(props.ascents.length / ROUTES_PER_PAGE)}
+      />
+    </Stack>
+  );
+}
