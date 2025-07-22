adf103c62b12e9ecf670d3c284cdf0bc1384aec1
Arne Goeteyn <goeteynarne@gmail.com>
2025-07-22 09:39:53 +0200

    attach route actions to routes table

---
+++ src/components/routes/routes-table.tsx
@@ -0,0 +1,63 @@
+import { RouteOverview } from "@/lib/routes/types";
+import { Edit, Trash, ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Button, Group, Pagination, Table } from "@mantine/core";
+import { useState } from "react";
+import RouteActions from "./route-actions";
+import { RouteContextProvider } from "@/contexts/route-context";
+
+type Props = {
+  routes: RouteOverview[];
+  showKind?: boolean;
+};
+
+const ROUTES_PER_PAGE = 25;
+
+export default function RouteTable(props: Props) {
+  const [page, setPage] = useState<number>(0);
+
+  const shownRoutes = () => {
+    return props.routes.slice(
+      page * ROUTES_PER_PAGE,
+      (page + 1) * ROUTES_PER_PAGE
+    );
+  };
+
+  return (
+    <>
+      <Table stickyHeader stickyHeaderOffset={60} striped>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Grade</Table.Th>
+            <Table.Th>Name</Table.Th>
+            {props.showKind && <Table.Th>Kind</Table.Th>}
+            <Table.Th>Ascents</Table.Th>
+            <Table.Th>Actions</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {shownRoutes().map((route) => (
+            <Table.Tr key={route.route.id}>
+              <Table.Td align="left" className="font-medium">
+                {route.route.grade}
+              </Table.Td>
+              <Table.Td>{route.route.name}</Table.Td>
+              {props.showKind && <Table.Td>{route.route.kind}</Table.Td>}
+              <Table.Td>{route.ascents.length}</Table.Td>
+              <Table.Td>
+                <RouteContextProvider routeId={route.route.id.toString()}>
+                  <RouteActions compact />
+                </RouteContextProvider>
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+      <Pagination
+        value={page + 1}
+        onChange={(p) => setPage(p - 1)}
+        total={Math.ceil(props.routes.length / ROUTES_PER_PAGE)}
+      />
+    </>
+  );
+}
