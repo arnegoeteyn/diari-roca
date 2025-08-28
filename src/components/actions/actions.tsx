99a8974c8e06ae2fda8eb572fe68ff075ec8f38f
Arne Goeteyn <goeteynarne@gmail.com>
2025-08-28 22:35:15 +0200

    Add button to add route on area page

--- src/components/actions/actions.tsx
+++ src/components/actions/actions.tsx
@@ -2,6 +2,7 @@ import { isActive } from "@/util";
 import SectorsActions from "@/components/actions/sectors-actions";
 import AscentsActions from "./ascents-actions";
 import RoutesActions from "./routes-actions";
+import AreaActions from "./area-actions";
 import { useParams } from "react-router-dom";
 
 type Props = {
@@ -21,6 +22,10 @@ export default function Actions(props: Props) {
   if (isActive(props.location, "sectors")) {
     const sectorIdNumber = Number(sectorId);
     return <SectorsActions sectorId={sectorIdNumber} />;
+  }
+
+  if (isActive(props.location, "areas")) {
+    return <AreaActions />;
   }
 
   return <div data-testid="no-actions"></div>;
