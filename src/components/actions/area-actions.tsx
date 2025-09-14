e9e0ad4501b2180c07a0d66f7fd3afda414b7d0d
Arne Goeteyn <goeteynarne@gmail.com>
2025-09-14 16:14:10 +0200

    remove usages of area-overview hook

--- src/components/actions/area-actions.tsx
+++ src/components/actions/area-actions.tsx
@@ -2,19 +2,35 @@ import { Loader, Stack } from "@mantine/core";
 import { useRoutesStore } from "@/hooks/store/use-store";
 import AddRouteButton from "../routes/add-route-button";
 import useSectorOverviews from "@/hooks/store/use-sector-overviews";
-import { ID } from "@/lib/routes";
-import useAreaOverview from "@/hooks/store/use-area-overview";
+import {
+  AreaOverview,
+  getArea,
+  ID,
+  sectorsForAreaWithCount,
+} from "@/lib/routes";
 import AddSectorButton from "../sectors/add-sector-button";
 
 type Props = {
   areaId: ID;
 };
 
+type AreaContent = AreaOverview;
+
+function useArea(areaId: ID): AreaContent {
+  const store = useRoutesStore((store) => store.store);
+  const area: AreaContent = {
+    ...getArea(store.data, areaId),
+    sectors: sectorsForAreaWithCount(store.data, areaId),
+  };
+
+  return area;
+}
+
 export default function AreaActions(props: Props) {
   const addRoute = useRoutesStore((store) => store.addRoute);
   const addSector = useRoutesStore((store) => store.addSector);
   const sectors = useSectorOverviews({ areaId: props.areaId });
-  const area = useAreaOverview(props.areaId);
+  const area = useArea(props.areaId);
 
   if (!sectors || !area) {
     return <Loader />;
@@ -22,7 +38,7 @@ export default function AreaActions(props: Props) {
   return (
     <Stack p={"16px"}>
       <AddSectorButton
-        areas={[area.area]}
+        areas={[area]}
         onSectorCreated={(sector) => addSector(sector).then()}
       />
       <AddRouteButton
