0dbf0303efc383e4df5cc6a98e470268660902d3
Arne Goeteyn <goeteynarne@gmail.com>
2025-10-26 10:35:18 +0100

    delete linked ascents in cache lib

--- src/lib/routes/types.ts
+++ src/lib/routes/types.ts
@@ -16,6 +16,16 @@ export type Store = {
 
 export type StoreData = Store["data"];
 
+export function NewStoreData() {
+  return {
+    areas: new Map<ID, Area>(),
+    sectors: new Map<ID, Sector>(),
+    routes: new Map<ID, Route>(),
+    ascents: new Map<ID, Ascent>(),
+    trips: new Map<ID, Trip>(),
+  } as StoreData;
+}
+
 export type ID = number;
 
 export type AppDate = string; // custom 'date' type since indexedDB has some difficulties with real date objects
